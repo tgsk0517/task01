d58952044a4c9923b71974ee90728879d3e419dc
tgsk0517 <51325943+tgsk0517@users.noreply.github.com>
2021-06-30 12:36:18 +0900

    1
    1

---
+++ task01/task01/Main.cpp
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+//////////////
+//第一問
+////////////////
+//struct Vector2
+//{
+//public:
+//	int x;
+//	int y;
+//};
+
+//////////////
+//第二問
+/////////////
+//struct Vector2
+//{
+//public:
+//	int Setx(int n) { return x = n; }
+//	int Sety(int n) { return y = n; }
+//
+//	int Getx() { return x; }
+//	int Gety() { return y; }
+//
+//private:
+//	int x;
+//	int y;
+//};
+
+////第三問
+//struct Vector3
+//{
+//	int Setx(int n) { return x = n; }
+//	int Sety(int n) { return y = n; }
+//	int Setz(int n) { return z = n; }
+//
+//
+//	int Getx() { return x; }
+//	int Gety() { return y; }
+//	int Getz() { return z; }
+//
+//private:
+//	int x;
+//	int y;
+//	int z;
+//};
+
+int main()
+{
+	////第一問の確認
+	//Vector2 vec2;
+	//vec2.x = 10;
+	//vec2.y = 5;
+	//printf("%d", vec2.x + vec2.y);
+
+	//////第二問の確認
+	//Vector2 vec2;
+	//vec2.Setx(10);
+	//vec2.Sety(5);
+	//printf("%d", vec2.Getx() + vec2.Gety());
+
+	////第三問の確認
+	//Vector3 vec3;
+	//vec3.Setx(10);
+	//vec3.Sety(5);
+	//vec3.Setz(100);
+	//printf("%d", vec3.Getx() + vec3.Gety() + vec3.Getz());
+
+}
